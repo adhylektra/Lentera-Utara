f747c9821ba35d29b05d71b44172096e38f6bbc2
Adhy Lektra <38153502+adhylektra@users.noreply.github.com>
2025-11-15 07:20:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Redirecting...</title>
+    <meta http-equiv="refresh" content="3;url=https://lenterautara.my.id/">
+</head>
+<body>
+    <p>
+      <image>
+    <img src="banner.jpg" width="1024" height="1024" alt=""/></p>
+    <h1><strong>Selamat Datang</strong><br>
+      Anda Saat ini menuju halaman <strong>Lentera Utara</strong><br>
+      selamat mebaca berita-berita menarik dari persembahan kami.
+      <a href="https://lenterautara.my.id/">di sini</a>.</h1>
+</body>
+</html>
